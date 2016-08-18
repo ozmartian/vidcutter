864b76bdfa25abc28c8fa625238b37c45c447123
Pete Alexandrou <pete@ozmartians.com>
2016-08-19 02:04:28 +0800

    ...

--- videocutter.py
+++ videocutter.py
@@ -8,7 +8,7 @@ from PyQt5.QtCore import QDir, Qt, QUrl, QFile, QFileInfo, QSize
 from PyQt5.QtGui import QIcon, QPalette
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import (QAction, QAbstractItemView, QApplication, QFileDialog, QHBoxLayout, QMainWindow,
+from PyQt5.QtWidgets import (QAction, QAbstractItemView, QApplication, QFileDialog, QHBoxLayout, QLabel, QMainWindow,
                              QSizePolicy, QSlider, QStyle, QStyleFactory, QListWidget, QListWidgetItem,
                              QToolBar, QToolButton, QVBoxLayout, QWidget, qApp)
 
@@ -55,11 +55,18 @@ class VideoCutter(QWidget):
         self.positionSlider = QSlider(Qt.Horizontal, sliderMoved=self.setPosition)
         self.positionSlider.setRange(0, 0)
         self.positionSlider.setStyleSheet('margin:8px 5px;')
+        self.timeCounter = QLabel('00:00:00')
+        self.timeCounter.setFixedWidth(85)
+        self.timeCounter.setStyleSheet('font-weight:bold; font-size:15px;')
+
+        sliderLayout = QHBoxLayout()
+        sliderLayout.addWidget(self.positionSlider)
+        sliderLayout.addWidget(self.timeCounter)
 
         self.zonelist = QListWidget()
         self.zonelist.setContextMenuPolicy(Qt.CustomContextMenu)
         self.zonelist.setFixedWidth(200)
-        listItem = QListWidgetItem("START => 00:04:35.000\nEND => 00:04:35.000")
+        listItem = QListWidgetItem('"START => 00:04:35.000\nEND => 00:04:35.000')
         listItem.setSizeHint(QSize(0, 50))
         self.zonelist.addItem(listItem)
 
@@ -71,20 +78,21 @@ class VideoCutter(QWidget):
         layout = QVBoxLayout()
         layout.setContentsMargins(10, 10, 10, 10)
         layout.addLayout(videoLayout)
-        layout.addWidget(self.positionSlider)
+        layout.addLayout(sliderLayout)
 
         self.muteButton = QToolButton(clicked=self.muteAudio)
         self.muteButton.setIcon(self.unmuteIcon)
         self.volumeSlider = QSlider(Qt.Horizontal, sliderMoved=self.setVolume)
+        self.volumeSlider.setValue(50)
         self.volumeSlider.setFixedWidth(100)
         self.volumeSlider.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Minimum)
         self.volumeSlider.setRange(0, 100)
 
         controlsLayout = QHBoxLayout()
+        controlsLayout.addWidget(self.lefttoolbar)
         controlsLayout.addStretch(1)
-        controlsLayout.addWidget(self.toolbar)
+        controlsLayout.addWidget(self.centertoolbar)
         controlsLayout.addStretch(1)
-        controlsLayout.addSpacing(10)
         controlsLayout.addWidget(self.muteButton)
         controlsLayout.addWidget(self.volumeSlider)
 
@@ -104,7 +112,7 @@ class VideoCutter(QWidget):
         self.cutStartIcon = QIcon(os.path.join(self.rootPath, 'icons', 'start.png'))
         self.cutEndIcon = QIcon(os.path.join(self.rootPath, 'icons', 'end.png'))
         self.saveIcon = QIcon(os.path.join(self.rootPath, 'icons', 'save.png'))
-        self.fscreenIcon = QIcon(os.path.join(self.rootPath, 'icons', 'fullscreen.png'))
+        # self.fscreenIcon = QIcon(os.path.join(self.rootPath, 'icons', 'fullscreen.png'))
         self.muteIcon = QIcon(os.path.join(self.rootPath, 'icons', 'muted.png'))
         self.unmuteIcon = QIcon(os.path.join(self.rootPath, 'icons', 'unmuted.png'))
 
@@ -112,29 +120,33 @@ class VideoCutter(QWidget):
         self.openAction = QAction(self.openIcon, 'Open', self, statusTip='Select video', triggered=self.openFile)
         self.playAction = QAction(self.playIcon, 'Play', self, statusTip='Play video', triggered=self.playVideo,
                                   enabled=False)
-        self.cutStartAction = QAction(self.cutStartIcon, 'Start', self, statusTip='Mark start of clip',
+        self.cutStartAction = QAction(self.cutStartIcon, 'Set Start', self, statusTip='Set clip start marker',
                                        triggered=self.cutStart, enabled=False)
-        self.cutEndAction = QAction(self.cutEndIcon, 'End', self, statusTip='Mark end of clip',
+        self.cutEndAction = QAction(self.cutEndIcon, 'Set End', self, statusTip='Set clip end marker',
                                      triggered=self.cutEnd, enabled=False)
-        self.saveAction = QAction(self.saveIcon, 'Save', self, statusTip='Save cuts to video file',
+        self.saveAction = QAction(self.saveIcon, 'Save', self, statusTip='Save scenes to new video file',
                                   triggered=self.saveVideo, enabled=False)
-        self.fscreenAction = QAction(self.fscreenIcon, 'Fullscreen', self, statusTip='View in fullscreen mode',
-                                        triggered=self.toggleFullscreen, enabled=False)
+        # self.fscreenAction = QAction(self.fscreenIcon, 'Fullscreen', self, statusTip='View in fullscreen mode',
+        #                              triggered=self.toggleFullscreen, enabled=False)
 
     def initToolbar(self):
-        self.toolbar = QToolBar()
-        self.toolbar.setFloatable(False)
-        self.toolbar.setMovable(False)
-        self.toolbar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
-        self.toolbar.setIconSize(QSize(26, 26))
-        self.toolbar.addAction(self.openAction)
-        self.toolbar.addAction(self.playAction)
-        self.toolbar.addAction(self.saveAction)
-        self.toolbar.addSeparator()
-        self.toolbar.addAction(self.cutStartAction)
-        self.toolbar.addAction(self.cutEndAction)
-        self.toolbar.addSeparator()
-        self.toolbar.addAction(self.fscreenAction)
+        self.lefttoolbar = QToolBar()
+        self.lefttoolbar.setFloatable(False)
+        self.lefttoolbar.setMovable(False)
+        self.lefttoolbar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        self.lefttoolbar.setIconSize(QSize(24, 24))
+        self.lefttoolbar.addAction(self.openAction)
+        self.lefttoolbar.addAction(self.playAction)
+        self.lefttoolbar.addAction(self.saveAction)
+        self.centertoolbar = QToolBar()
+        self.centertoolbar.setFloatable(False)
+        self.centertoolbar.setMovable(False)
+        self.centertoolbar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        self.centertoolbar.setIconSize(QSize(24, 24))
+        self.centertoolbar.addWidget(QLabel('Set Start'))
+        self.centertoolbar.addAction(self.cutStartAction)
+        self.centertoolbar.addAction(self.cutEndAction)
+        # self.centertoolbar.addAction(self.fscreenAction)
 
     def openFile(self):
         fileName, _ = QFileDialog.getOpenFileName(parent=self.parent, caption='Select video', directory=QDir.homePath())
@@ -152,9 +164,11 @@ class VideoCutter(QWidget):
         self.playAction.setEnabled(flag)
         self.cutStartAction.setEnabled(flag)
         self.cutEndAction.setEnabled(flag)
-        self.fscreenAction.setEnabled(flag)
+        # self.fscreenAction.setEnabled(flag)
 
     def setPosition(self, position):
+        if self.mediaPlayer.isVideoAvailable():
+            self.mediaPlayer.pause()
         self.mediaPlayer.setPosition(position)
 
     def positionChanged(self, position):
@@ -166,7 +180,7 @@ class VideoCutter(QWidget):
             self.playAction.setText('Pause')
         else:
             self.playAction.setIcon(self.playIcon)
-            self.playAction.setText('Play ')
+            self.playAction.setText('Play')
 
     def durationChanged(self, duration):
         self.positionSlider.setRange(0, duration)
