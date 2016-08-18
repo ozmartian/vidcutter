6c2b9ffbfc9aab1d6d67212cba07995f996acc95
Pete Alexandrou <pete@ozmartians.com>
2016-08-18 22:39:00 +0800

    improved artwork and layout nearly complete

--- videocutter.py
+++ videocutter.py
@@ -1,16 +1,40 @@
-#!/usr/bin/env python
+    #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os
 import sys
 
 from PyQt5.QtCore import QDir, Qt, QUrl, QFile, QFileInfo, QSize
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QPalette
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtWidgets import (QAction, QAbstractItemView, QApplication, QFileDialog, QHBoxLayout, QMainWindow,
-                             QPushButton, QSizePolicy, QSlider, QStyle, QStyleFactory, QStyleOptionButton, QTableWidget,
-                             QTableWidgetItem, QToolBar, QVBoxLayout, QWidget, qApp)
+                             QSizePolicy, QSlider, QStyle, QStyleFactory, QListWidget, QListWidgetItem,
+                             QToolBar, QToolButton, QVBoxLayout, QWidget, qApp)
+
+
+class VideoWidget(QVideoWidget):
+    def __init__(self, parent=None):
+        super(VideoWidget, self).__init__(parent)
+        self.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        p = self.palette()
+        p.setColor(QPalette.Window, Qt.black)
+        self.setPalette(p)
+        self.setAttribute(Qt.WA_OpaquePaintEvent)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape and self.isFullScreen():
+            self.setFullScreen(False)
+            event.accept()
+        elif event.key() == Qt.Key_Enter and not self.isFullScreen():
+            self.setFullScreen(not self.isFullScreen())
+            event.accept()
+        else:
+            super(VideoWidget, self).keyPressEvent(event)
+
+    def mouseDoubleClickEvent(self, event):
+        self.setFullScreen(not self.isFullScreen())
+        event.accept()
 
 
 class VideoCutter(QWidget):
@@ -18,62 +42,53 @@ class VideoCutter(QWidget):
         super(VideoCutter, self).__init__(parent)
         self.parent = parent
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
-
-        self.videoWidget = QVideoWidget()
+        self.videoWidget = VideoWidget()
 
         self.rootPath = QFileInfo(__file__).absolutePath()
 
-        self.openIcon = QIcon(os.path.join(self.rootPath, 'icons', 'open.png'))
-        self.playIcon = QIcon(os.path.join(self.rootPath, 'icons', 'play.png'))
-        self.pauseIcon = QIcon(os.path.join(self.rootPath, 'icons', 'pause.png'))
-        self.markStartIcon = QIcon(os.path.join(self.rootPath, 'icons', 'start.png'))
-        self.markEndIcon = QIcon(os.path.join(self.rootPath, 'icons', 'end.png'))
-        self.saveIcon = QIcon(os.path.join(self.rootPath, 'icons', 'save.png'))
-
+        self.initIcons()
         self.initActions()
         self.initToolbar()
 
-        # MainWindow.loadStyleSheet(os.path.join(self.rootPath, 'qss', 'qslider.qss'))
+        # MainWindow.loadStyleSheet(filepath=os.path.join(self.rootPath, 'qss', 'qslider.qss'))
 
-        self.positionSlider = QSlider(Qt.Horizontal)
-        self.positionSlider.setStyle(QStyleFactory.create('Oxygen'))
-        self.positionSlider.setRange(0, 1000)
-        # self.positionSlider.setTickPosition(QSlider.TicksBelow)
-        # self.positionSlider.setTickInterval(5)
-        self.positionSlider.sliderMoved.connect(self.setPosition)
+        self.positionSlider = QSlider(Qt.Horizontal, sliderMoved=self.setPosition)
+        self.positionSlider.setRange(0, 0)
+        self.positionSlider.setStyleSheet('margin:8px 5px;')
 
-        # self.errorLabel = QLabel()
-        # self.errorLabel.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
-
-        self.timelist = QTableWidget()
-        self.timelist.setFixedWidth(200)
-        self.timelist.horizontalScrollBar().setVisible(False)
-        self.timelist.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
-        self.timelist.setColumnCount(2)
-        self.timelist.setRowCount(2)
-        self.timelist.verticalHeader().setVisible(False)
-        self.timelist.setHorizontalHeaderLabels(['Position', 'Time'])
-        self.timelist.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.timelist.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.timelist.setSelectionMode(QAbstractItemView.MultiSelection)
-        self.timelist.setItem(0, 0, QTableWidgetItem('start'))
-        self.timelist.setItem(0, 1, QTableWidgetItem('00:04:03'))
-        self.timelist.setItem(1, 0, QTableWidgetItem('end'))
-        self.timelist.setItem(1, 1, QTableWidgetItem('00:12:42'))
+        self.zonelist = QListWidget()
+        self.zonelist.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.zonelist.setFixedWidth(200)
+        listItem = QListWidgetItem("START => 00:04:35.000\nEND => 00:04:35.000")
+        listItem.setSizeHint(QSize(0, 50))
+        self.zonelist.addItem(listItem)
 
         videoLayout = QHBoxLayout()
         videoLayout.setContentsMargins(0, 0, 0, 0)
         videoLayout.addWidget(self.videoWidget)
-        videoLayout.addWidget(self.timelist)
+        videoLayout.addWidget(self.zonelist)
 
         layout = QVBoxLayout()
         layout.setContentsMargins(10, 10, 10, 10)
-        layout.setAlignment(Qt.AlignCenter)
         layout.addLayout(videoLayout)
-        layout.addWidget(self.toolbar)
         layout.addWidget(self.positionSlider)
-        # layout.addWidget(self.errorLabel)
 
+        self.muteButton = QToolButton(clicked=self.muteAudio)
+        self.muteButton.setIcon(self.unmuteIcon)
+        self.volumeSlider = QSlider(Qt.Horizontal, sliderMoved=self.setVolume)
+        self.volumeSlider.setFixedWidth(100)
+        self.volumeSlider.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Minimum)
+        self.volumeSlider.setRange(0, 100)
+
+        controlsLayout = QHBoxLayout()
+        controlsLayout.addStretch(1)
+        controlsLayout.addWidget(self.toolbar)
+        controlsLayout.addStretch(1)
+        controlsLayout.addSpacing(10)
+        controlsLayout.addWidget(self.muteButton)
+        controlsLayout.addWidget(self.volumeSlider)
+
+        layout.addLayout(controlsLayout)
         self.setLayout(layout)
 
         self.mediaPlayer.setVideoOutput(self.videoWidget)
@@ -82,42 +97,62 @@ class VideoCutter(QWidget):
         self.mediaPlayer.durationChanged.connect(self.durationChanged)
         self.mediaPlayer.error.connect(self.handleError)
 
+    def initIcons(self):
+        self.openIcon = QIcon(os.path.join(self.rootPath, 'icons', 'open.png'))
+        self.playIcon = QIcon(os.path.join(self.rootPath, 'icons', 'play.png'))
+        self.pauseIcon = QIcon(os.path.join(self.rootPath, 'icons', 'pause.png'))
+        self.cutStartIcon = QIcon(os.path.join(self.rootPath, 'icons', 'start.png'))
+        self.cutEndIcon = QIcon(os.path.join(self.rootPath, 'icons', 'end.png'))
+        self.saveIcon = QIcon(os.path.join(self.rootPath, 'icons', 'save.png'))
+        self.fscreenIcon = QIcon(os.path.join(self.rootPath, 'icons', 'fullscreen.png'))
+        self.muteIcon = QIcon(os.path.join(self.rootPath, 'icons', 'muted.png'))
+        self.unmuteIcon = QIcon(os.path.join(self.rootPath, 'icons', 'unmuted.png'))
+
     def initActions(self):
         self.openAction = QAction(self.openIcon, 'Open', self, statusTip='Select video', triggered=self.openFile)
         self.playAction = QAction(self.playIcon, 'Play', self, statusTip='Play video', triggered=self.playVideo,
                                   enabled=False)
-        self.markStartAction = QAction(self.markStartIcon, 'Mark Start', self, statusTip='Mark start of clip',
-                                       triggered=self.markStart, enabled=False)
-        self.markEndAction = QAction(self.markEndIcon, 'Mark End', self, statusTip='Mark end of clip',
-                                     triggered=self.markEnd, enabled=False)
-        self.saveAction = QAction(self.saveIcon, 'Save video', self, statusTip='Save cuts to video file',
+        self.cutStartAction = QAction(self.cutStartIcon, 'Start', self, statusTip='Mark start of clip',
+                                       triggered=self.cutStart, enabled=False)
+        self.cutEndAction = QAction(self.cutEndIcon, 'End', self, statusTip='Mark end of clip',
+                                     triggered=self.cutEnd, enabled=False)
+        self.saveAction = QAction(self.saveIcon, 'Save', self, statusTip='Save cuts to video file',
                                   triggered=self.saveVideo, enabled=False)
+        self.fscreenAction = QAction(self.fscreenIcon, 'Fullscreen', self, statusTip='View in fullscreen mode',
+                                        triggered=self.toggleFullscreen, enabled=False)
 
     def initToolbar(self):
         self.toolbar = QToolBar()
-        self.toolbar.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
         self.toolbar.setFloatable(False)
         self.toolbar.setMovable(False)
         self.toolbar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
-        self.toolbar.setIconSize(QSize(24, 24))
+        self.toolbar.setIconSize(QSize(26, 26))
         self.toolbar.addAction(self.openAction)
         self.toolbar.addAction(self.playAction)
-        self.toolbar.addAction(self.markStartAction)
-        self.toolbar.addAction(self.markEndAction)
         self.toolbar.addAction(self.saveAction)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.cutStartAction)
+        self.toolbar.addAction(self.cutEndAction)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.fscreenAction)
 
     def openFile(self):
         fileName, _ = QFileDialog.getOpenFileName(parent=self.parent, caption='Select video', directory=QDir.homePath())
         if fileName != '':
             self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(fileName)))
-            self.playAction.setEnabled(True)
-            self.markStartAction.setEnabled(True)
+            self.enableMediaControls(True)
 
     def playVideo(self):
         if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
             self.mediaPlayer.pause()
         else:
             self.mediaPlayer.play()
+
+    def enableMediaControls(self, flag=True):
+        self.playAction.setEnabled(flag)
+        self.cutStartAction.setEnabled(flag)
+        self.cutEndAction.setEnabled(flag)
+        self.fscreenAction.setEnabled(flag)
 
     def setPosition(self, position):
         self.mediaPlayer.setPosition(position)
@@ -131,15 +166,29 @@ class VideoCutter(QWidget):
             self.playAction.setText('Pause')
         else:
             self.playAction.setIcon(self.playIcon)
-            self.playAction.setText('Play')
+            self.playAction.setText('Play ')
 
     def durationChanged(self, duration):
         self.positionSlider.setRange(0, duration)
 
-    def markStart(self):
+    def muteAudio(self, muted):
+        if self.mediaPlayer.isMuted():
+            self.mediaPlayer.setMuted(not self.mediaPlayer.isMuted())
+            self.muteButton.setIcon(self.unmuteIcon)
+        else:
+            self.mediaPlayer.setMuted(not self.mediaPlayer.isMuted())
+            self.muteButton.setIcon(self.muteIcon)
+
+    def setVolume(self, volume):
+        self.mediaPlayer.setVolume(volume)
+
+    def toggleFullscreen(self):
+        self.videoWidget.setFullScreen(not self.videoWidget.isFullScreen())
+
+    def cutStart(self):
         pass
 
-    def markEnd(self):
+    def cutEnd(self):
         pass
 
     def saveVideo(self):
@@ -152,19 +201,20 @@ class VideoCutter(QWidget):
 class MainWindow(QMainWindow):
     def __init__(self, parent=None, flags=Qt.Window):
         super(MainWindow, self).__init__(parent, flags)
-        self.cutter = VideoCutter(self)
-        self.statusBar()
-        self.setCentralWidget(self.cutter)
+        self.statusBar().showMessage('Ready')
+        self.setCentralWidget(VideoCutter(self))
         self.setWindowTitle('VideoCutter')
         self.setWindowIcon(QIcon(os.path.join(QFileInfo(__file__).absolutePath(), 'icons', 'app.png')))
         self.resize(800, 600)
 
     @staticmethod
-    def loadStyleSheet(sheetFile):
-        file = QFile(sheetFile)
-        file.open(QFile.ReadOnly)
-        styleSheet = str(file.readAll(), encoding='utf8')
-        qApp.setStyleSheet(styleSheet)
+    def loadStyleSheet(filepath=None, stylesheet=None):
+        if filepath is not None:
+            stylefile = QFile(filepath)
+            stylefile.open(QFile.ReadOnly)
+            stylesheet = str(stylefile.readAll(), encoding='utf8')
+        if stylesheet is not None:
+            qApp.setStyleSheet(stylesheet)
 
 
 if __name__ == '__main__':
--- videowidget.py
+++ videowidget.py
@@ -1,24 +1,65 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
+
+#############################################################################
+##
+## Copyright (C) 2013 Riverbank Computing Limited.
+## Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+## All rights reserved.
+##
+## This file is part of the examples of PyQt.
+##
+## $QT_BEGIN_LICENSE:BSD$
+## You may use this file under the terms of the BSD license as follows:
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+##   * Redistributions of source code must retain the above copyright
+##     notice, this list of conditions and the following disclaimer.
+##   * Redistributions in binary form must reproduce the above copyright
+##     notice, this list of conditions and the following disclaimer in
+##     the documentation and/or other materials provided with the
+##     distribution.
+##   * Neither the name of Nokia Corporation and its Subsidiary(-ies) nor
+##     the names of its contributors may be used to endorse or promote
+##     products derived from this software without specific prior written
+##     permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+## $QT_END_LICENSE$
+##
+#############################################################################
+
 
 from PyQt5.QtCore import QDir, Qt, QUrl
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel, QPushButton, QSizePolicy, QSlider, QStyle,
-                             QVBoxLayout, QWidget)
+from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
+        QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget)
 
 
 class VideoPlayer(QWidget):
-    def __init__(self, parent=None, flags=Qt.Window):
-        super(VideoPlayer, self).__init__(parent, flags)
+
+    def __init__(self, parent=None):
+        super(VideoPlayer, self).__init__(parent)
 
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
 
         videoWidget = QVideoWidget()
 
-        openButton = QPushButton()
-        openButton.setIcon(self.style().standardIcon(QStyle.SP_FileIcon))
-        openButton.clicked.connect(self.parent.openFile)
+        openButton = QPushButton("Open...")
+        openButton.clicked.connect(self.openFile)
 
         self.playButton = QPushButton()
         self.playButton.setEnabled(False)
@@ -27,14 +68,14 @@ class VideoPlayer(QWidget):
 
         self.positionSlider = QSlider(Qt.Horizontal)
         self.positionSlider.setRange(0, 0)
-        self.positionSlider.setTickInterval(5)
         self.positionSlider.sliderMoved.connect(self.setPosition)
 
         self.errorLabel = QLabel()
-        self.errorLabel.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        self.errorLabel.setSizePolicy(QSizePolicy.Preferred,
+                QSizePolicy.Maximum)
 
         controlLayout = QHBoxLayout()
-        controlLayout.setContentsMargins(10, 10, 10, 10)
+        controlLayout.setContentsMargins(0, 0, 0, 0)
         controlLayout.addWidget(openButton)
         controlLayout.addWidget(self.playButton)
         controlLayout.addWidget(self.positionSlider)
@@ -52,6 +93,15 @@ class VideoPlayer(QWidget):
         self.mediaPlayer.durationChanged.connect(self.durationChanged)
         self.mediaPlayer.error.connect(self.handleError)
 
+    def openFile(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Movie",
+                QDir.homePath())
+
+        if fileName != '':
+            self.mediaPlayer.setMedia(
+                    QMediaContent(QUrl.fromLocalFile(fileName)))
+            self.playButton.setEnabled(True)
+
     def play(self):
         if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
             self.mediaPlayer.pause()
@@ -61,10 +111,10 @@ class VideoPlayer(QWidget):
     def mediaStateChanged(self, state):
         if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
             self.playButton.setIcon(
-                self.style().standardIcon(QStyle.SP_MediaPause))
+                    self.style().standardIcon(QStyle.SP_MediaPause))
         else:
             self.playButton.setIcon(
-                self.style().standardIcon(QStyle.SP_MediaPlay))
+                    self.style().standardIcon(QStyle.SP_MediaPlay))
 
     def positionChanged(self, position):
         self.positionSlider.setValue(position)
@@ -81,12 +131,13 @@ class VideoPlayer(QWidget):
 
 
 if __name__ == '__main__':
+
     import sys
 
     app = QApplication(sys.argv)
 
     player = VideoPlayer()
-    player.resize(800, 600)
+    player.resize(320, 240)
     player.show()
 
     sys.exit(app.exec_())
