24e35ec5d765833d3e2ea0c882381066d60fdc7e
Pete Alexandrou <pete@ozmartians.com>
2016-12-15 10:09:56 +0800

    win pyinstaller script updates

--- _build/pyinstaller/build.pyinstaller.win.bat
+++ _build/pyinstaller/build.pyinstaller.win.bat
@@ -6,7 +6,17 @@ if [%1]==[] (
     SET ARCH=%1
 )
 
+if ["%ARCH%"]==["64"] (
+    SET BINARCH=x64
+)
+if ["%ARCH%"]==["32"] (
+    SET BINARCH=x86
+)
 
 rd /s /q build
 rd /s /q dist
+del ..\..\bin\ffmpeg.exe
+
+unzip ..\..\bin\%BINARCH%\ffmpeg.zip -d ..\..\bin\
+
 pyinstaller --clean vidcutter.win%ARCH%.spec
