06eb561516f076087551157494dcdf4781a7b983
Pete Alexandrou <pete@ozmartians.com>
2017-01-09 12:04:33 +0800

    deb package to current version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def get_install_requires():
     if packager == 'pypi':
         return ['PyQt5 >= 5.5', 'qtawesome']
     else:
-        return []
+        return ['qtawesome']
 
 
 def get_package_data():
