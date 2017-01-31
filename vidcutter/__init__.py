a4f87a2ee9dd17315f98ac1d02ee0bf41544a57b
Pete Alexandrou <pete@ozmartians.com>
2017-01-31 09:07:52 +0800

    submodules sync

--- vidcutter/__init__.py
+++ vidcutter/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = '2.6.5'
+__version__ = '2.5.0'
 
-__packager__ = 'arch'  # [ pypi | arch | deb ]
+__packager__ = 'pypi'  # [ pypi | arch | deb ]
