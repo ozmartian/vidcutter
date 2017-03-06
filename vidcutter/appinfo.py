4e89778f702dde47476f6a797b5d749010fd07a2
Pete Alexandrou <pete@ozmartians.com>
2017-03-06 09:23:11 +0800

    increase tabwidget height to avoid scrolling in windows

--- vidcutter/appinfo.py
+++ vidcutter/appinfo.py
@@ -54,7 +54,7 @@ class AppInfo(QDialog):
         self.setLayout(layout)
         self.setWindowTitle('About %s' % qApp.applicationName())
         self.setWindowIcon(self.parent.windowIcon())
-        self.setMinimumSize(585, 420)
+        self.setMinimumSize(585, 430)
 
     def closeEvent(self, event: QCloseEvent):
         self.tab_about.deleteLater()
