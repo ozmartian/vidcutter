5d36496f497efe4aab6a8c3f9d3cb76042507117
Pete Alexandrou <pete@ozmartians.com>
2017-05-31 12:28:56 +0800

    Chocolatey scripts updated for 3.5.0 release

--- _packaging/chocolatey/tools/chocolateyinstall.ps1
+++ _packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'vidcutter'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/ozmartian/vidcutter/releases/download/3.2.0/VidCutter-3.2.0-setup-x86.exe'
-$url64      = 'https://github.com/ozmartian/vidcutter/releases/download/3.2.0/VidCutter-3.2.0-setup-x64.exe'
+$url        = 'https://github.com/ozmartian/vidcutter/releases/download/3.5.0/VidCutter-3.5.0-setup-win32.exe'
+$url64      = 'https://github.com/ozmartian/vidcutter/releases/download/3.5.0/VidCutter-3.5.0-setup-win64.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,9 +15,9 @@ $packageArgs = @{
 
   softwareName  = 'VidCutter'
 
-  checksum      = '82f49f5770fa37fe2450136256560148c36b6b4d56c6945ba0b709f94a2fcbce'
+  checksum      = 'c080b31dd06952a55104cfdaf39ec314844c6529b068d0d4fadee37d5b953818'
   checksumType  = 'sha256'
-  checksum64    = 'fb6f233bb31da7901ce6a663c9f74d1f5abad509d4616502e4e0b6a34deb0f52'
+  checksum64    = '74c96e6fb10a42af9e1264db6d3d5a630b30ef2652ede37eabef81d6bd249ef5'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
