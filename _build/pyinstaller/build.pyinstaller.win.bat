7df8fa999e7f5a8034e96264a2959afb29566aef
Pete Alexandrou <pete@ozmartians.com>
2017-07-21 19:51:15 +0800

    appveyor testing

--- _build/pyinstaller/build.pyinstaller.win.bat
+++ _build/pyinstaller/build.pyinstaller.win.bat
@@ -10,13 +10,15 @@ if [%1]==[] (
 
 if ["%ARCH%"]==["64"] (
     SET BINARCH=x64
-    SET FFMPEG=https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.7z
+    SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.7z
+    SET FFMPEG=ffmpeg-latest-win64-static.7z
     SET MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/0.7.97/MediaInfo_CLI_0.7.97_Windows_x64.zip
     SET MEDIAINFO=MediaInfo_CLI_0.7.97_Windows_x64.zip
 )
 if ["%ARCH%"]==["32"] (
     SET BINARCH=x86
-    SET FFMPEG=https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.7z
+    SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.7z
+    SET FFMPEG=ffmpeg-latest-win32-static.7z
     SET MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/0.7.97/MediaInfo_CLI_0.7.97_Windows_i386.zip
     SET MEDIAINFO=MediaInfo_CLI_0.7.97_Windows_i386.zip
 )
@@ -35,8 +37,8 @@ del /q ..\..\bin\*.*
 REM ......................download latest FFmpeg static binary......................
 
 if not exist ".\temp\" mkdir temp
-if not exist "temp\ffmpeg-latest-win%ARCH%-static.7z" ( aria2c -d temp -x 6 %FFMPEG% )
-if not exist "temp\%MEDIAINFO%" ( aria2c -d temp -x 6 %MEDIAINFO_URL% )
+if not exist "temp\ffmpeg-latest-win%ARCH%-static.7z" ( curl -L -fsS -o %FFMPEG% "%FFMPEG_URL%" )
+if not exist "temp\%MEDIAINFO%" ( curl -L -fsS -o %MEDIAINFO% "%MEDIAINFO_URL%" )
 
 REM ......................extract ffmpeg.exe to its expected location......................
 
