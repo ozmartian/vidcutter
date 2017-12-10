233224b0ec1f7dd2d0dc2ee1c561d06ea0dd6e89
Pete Alexandrou <pete@ozmartians.com>
2017-12-10 21:26:47 +0800

    create VERSION file during AppImage build

--- _packaging/appimage/build.sh
+++ _packaging/appimage/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+export NO_GLIBC_VERSION="1"
+
 wget https://github.com/AppImage/AppImages/raw/master/pkg2appimage
 chmod +x ./pkg2appimage
 
-export NO_GLIBC_VERSION="1"
-export VERSION="${BUILD_VERSION}"
+if [ -z ${BUILD_VERSION} ]; then
+	echo "$(python ../../_build/pyinstaller/version.py)" > VERSION
+else
+    echo "${BUILD_VERSION}" > VERSION
+fi
 
 ./pkg2appimage VidCutter.yml
 rm ./pkg2appimage
+rm ./VERSION
