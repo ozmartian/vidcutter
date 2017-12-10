d3673d43b211d5e8677f514ddb7a7ddb9da8e5fa
Pete Alexandrou <pete@ozmartians.com>
2017-12-10 22:26:08 +0800

    appimage script updates for Travis CI

--- _packaging/appimage/build.sh
+++ _packaging/appimage/build.sh
@@ -6,5 +6,4 @@ wget https://github.com/AppImage/AppImages/raw/master/pkg2appimage
 chmod +x ./pkg2appimage
 
 ./pkg2appimage VidCutter.yml
-mv ./out/VidCutter--x86_64.AppImage ./out/VidCutter-${BUILD_VERSION}-x64.AppImage
 rm ./pkg2appimage
