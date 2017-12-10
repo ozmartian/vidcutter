ca883c1a3dc98d2e0e3b674cd8597620076a1838
Pete Alexandrou <pete@ozmartians.com>
2017-12-10 20:03:50 +0800

    travis ci script moved to new vidcutter-appimage repo

--- _packaging/appimage/build.sh
+++ _packaging/appimage/build.sh
@@ -3,9 +3,9 @@
 wget https://github.com/AppImage/AppImages/raw/master/pkg2appimage
 chmod +x ./pkg2appimage
 
-NO_GLIBC_VERSION="1"
+export NO_GLIBC_VERSION="1"
 ./pkg2appimage VidCutter.yml
 rm ./pkg2appimage
 
-BUILD_VERSION="$(python3 ../../_build/pyinstaller/version.py)"
+export BUILD_VERSION="$(python3 ../../_build/pyinstaller/version.py)"
 [ -r out/VidCutter-*.AppImage ] && mv out/VidCutter-*-x86_64.AppImage out/VidCutter-${BUILD_VERSION}-x64.AppImage
