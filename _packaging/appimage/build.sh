c71cb1d09b29632daad1d7b61732967458a9e564
Pete Alexandrou <pete@ozmartians.com>
2018-01-05 06:43:40 +0800

    AppImage build script updates

--- _packaging/appimage/build.sh
+++ _packaging/appimage/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+export ARCH="x86_64"
 export NO_GLIBC_VERSION="1"
+export VERSION="5.5.0"
 
 wget https://github.com/AppImage/AppImages/raw/master/pkg2appimage
 chmod +x ./pkg2appimage
 
-bash -c "./pkg2appimage ./VidCutter.yml"
+./pkg2appimage ./VidCutter.yml
+rm ./pkg2appimage
 
+mv out/VidCutter--${ARCH}.AppImage out/VidCutter-${VERSION}-x64.AppImage
