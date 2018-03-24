18181adbf74f91cb0c851ff8d2ce92f610eb3464
Pete Alexandrou <pete@ozmartians.com>
2018-03-24 08:29:02 +0800

    first version of Homebrew Cask formulae

---
+++ _packaging/homebrew/vidcutter.rb
@@ -0,0 +1,12 @@
+cask 'vidcutter' do
+  version '5.5.0'
+  sha256 '66ec76e878ccf70a7ebadda94f4b599426201cc3b30b52c42d84c4a7142e6f1b'
+
+  url 'https://github.com/ozmartian/vidcutter/releases/download/{version}/VidCutter-{version}-macOS.dmg'
+  appcast 'https://github.com/ozmartian/vidcutter/releases.atom',
+          checkpoint: '249e31bf274b71943c530f7d8da4a069c8d82ac56141a583fd48f3d1aa17d092'
+  name 'VidCutter'
+  homepage 'https://vidcutter.ozmartians.com'
+
+  app 'VidCutter.app'
+end
