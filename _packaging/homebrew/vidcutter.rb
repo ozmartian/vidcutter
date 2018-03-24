7955074dec423cf7c44e5a60d35961ad473ae4c2
Pete Alexandrou <pete@ozmartians.com>
2018-03-24 15:11:25 +0800

    hardcode version number, using variable / named parameter was breaking brew cask for some reason

--- _packaging/homebrew/vidcutter.rb
+++ _packaging/homebrew/vidcutter.rb
@@ -2,7 +2,7 @@ cask 'vidcutter' do
   version '5.5.0'
   sha256 '66ec76e878ccf70a7ebadda94f4b599426201cc3b30b52c42d84c4a7142e6f1b'
 
-  url 'https://github.com/ozmartian/vidcutter/releases/download/{version}/VidCutter-{version}-macOS.dmg'
+  url 'https://github.com/ozmartian/vidcutter/releases/download/5.5.0/VidCutter-5.5.0-macOS.dmg'
   appcast 'https://github.com/ozmartian/vidcutter/releases.atom',
           checkpoint: '249e31bf274b71943c530f7d8da4a069c8d82ac56141a583fd48f3d1aa17d092'
   name 'VidCutter'
