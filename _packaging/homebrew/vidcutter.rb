353428e38f7930256534cb98b883f0b748a6eab6
Pete Alexandrou <pete@ozmartians.com>
2018-08-17 15:52:32 +1000

    final homebrew updates

--- _packaging/homebrew/vidcutter.rb
+++ _packaging/homebrew/vidcutter.rb
@@ -3,11 +3,10 @@ cask 'vidcutter' do
   sha256 '33943d56de307b026554824bc9b06779428bd75e5197779897ee2b4952313961'
 
   # github.com/ozmartian/vidcutter was verified as official when first introduced to the cask
-  url 'https://github.com/ozmartian/vidcutter/releases/download/#{version}/VidCutter-#{version}-macOS.dmg'
-  appcast 'https://github.com/ozmartian/vidcutter/releases.atom',
-          checkpoint: '249e31bf274b71943c530f7d8da4a069c8d82ac56141a583fd48f3d1aa17d092'
+  url "https://github.com/ozmartian/vidcutter/releases/download/#{version}/VidCutter-#{version}-macOS.dmg"
+  appcast 'https://github.com/ozmartian/vidcutter/releases.atom'
   name 'VidCutter'
-  homepage 'https://vidcutter.ozmartians.com'
+  homepage 'https://vidcutter.ozmartians.com/'
 
   app 'VidCutter.app'
 end
