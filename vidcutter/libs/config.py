e7260e1f05b1c69e812194106aeeb549c088480e
Philipp Matthias Hahn <pmhahn@pmhahn.de>
2018-11-13 20:51:22 +0100

    Parse command arguments as list
    instead of string using broken shell escaping.

--- vidcutter/libs/config.py
+++ vidcutter/libs/config.py
@@ -68,9 +68,9 @@ class Config:
     @property
     def encoding(self) -> dict:
         return {
-            'hevc': 'libx265 -tune zerolatency -preset ultrafast -x265-params crf=23 -qp 4 -flags +cgop',
-            'h264': 'libx264 -tune film -preset ultrafast -x264-params crf=23 -qp 0 -flags +cgop',
-            'vp9': 'libvpx-vp9 -deadline best -quality best'
+            'hevc': ['libx265', '-tune', 'zerolatency', '-preset', 'ultrafast', '-x265-params', 'crf=23', '-qp', '4', '-flags', '+cgop'],
+            'h264': ['libx264', '-tune', 'film', '-preset', 'ultrafast', '-x264-params', 'crf=23', '-qp', '0', '-flags', '+cgop'],
+            'vp9': ['libvpx-vp9', '-deadline', 'best', '-quality', 'best'],
         }
 
     @property
