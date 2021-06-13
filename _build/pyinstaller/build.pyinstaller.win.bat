7d8cd669c6262558e2c8010662a0646f3ddc19cc
Pete Alexandrou <pete@ozmartians.com>
2021-06-13 17:19:39 +1000

    Preparations for new Windows x64 build

--- _build/pyinstaller/build.pyinstaller.win.bat
+++ _build/pyinstaller/build.pyinstaller.win.bat
@@ -11,20 +11,20 @@ if [%1]==[] (
 
 if ["%ARCH%"]==["64"] (
     SET BINARCH=x64
-    SET PYPATH=C:\Python36-x64
-    SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win64/shared/ffmpeg-latest-win64-shared.zip
-    SET FFMPEG=ffmpeg-latest-win64-shared.zip
-    SET MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/18.03.1/MediaInfo_CLI_18.03.1_Windows_x64.zip
-    SET MEDIAINFO=MediaInfo_CLI_18.03.1_Windows_x64.zip
+    SET PYPATH=C:\Python39
+    SET FFMPEG_URL=https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-4.4-full_build.7z
+    SET FFMPEG=ffmpeg-4.4-full_build.7z
+    SET MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/21.03/MediaInfo_CLI_21.03_Windows_x64.zip
+    SET MEDIAINFO=MediaInfo_CLI_21.03_Windows_x64.zip
 )
-if ["%ARCH%"]==["32"] (
-    SET BINARCH=x86
-    SET PYPATH=C:\Python35
-    SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win32/shared/ffmpeg-latest-win32-shared.zip
-    SET FFMPEG=ffmpeg-latest-win32-shared.zip
-    SET MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/18.03.1/MediaInfo_CLI_18.03.1_Windows_i386.zip
-    SET MEDIAINFO=MediaInfo_CLI_18.03.1_Windows_i386.zip
-)
+rem if ["%ARCH%"]==["32"] (
+rem     SET BINARCH=x86
+rem     SET PYPATH=C:\Python35
+rem     SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win32/shared/ffmpeg-latest-win32-shared.zip
+rem     SET FFMPEG=ffmpeg-latest-win32-shared.zip
+rem     SET MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/18.03.1/MediaInfo_CLI_18.03.1_Windows_i386.zip
+rem     SET MEDIAINFO=MediaInfo_CLI_18.03.1_Windows_i386.zip
+rem )
 
 REM ......................get latest version number......................
 for /f "delims=" %%a in ('%PYPATH%\python.exe version.py') do @set APPVER=%%a
@@ -36,15 +36,15 @@ if not exist "..\..\bin\" ( mkdir ..\..\bin\ ) else ( del /q ..\..\bin\*.* )
 
 REM ......................download latest FFmpeg & MediaInfo shared binary + libs ......................
 if not exist ".\temp\" mkdir temp
-if not exist "temp\ffmpeg-latest-win%ARCH%-shared.7z" ( call curl -k -L -# -o temp\%FFMPEG% "%FFMPEG_URL%" )
+if not exist "temp\%FFMPEG%" ( call curl -k -L -# -o temp\%FFMPEG% "%FFMPEG_URL%" )
 if not exist "temp\%MEDIAINFO%" ( call curl -k -L -# -o temp\%MEDIAINFO% "%MEDIAINFO_URL%" )
 
 REM ......................extract files & move them to top-level binary folder ......................
 cd temp\
-7z x ffmpeg-latest-win%ARCH%-shared.7z ffmpeg-latest-win%ARCH%-shared\bin
-del /q ffmpeg-latest-win%ARCH%-shared\bin\ffplay.exe
+7z x "%FFMPEG%" ffmpeg-4.4-full_build\bin
+del /q ffmpeg-4.4-full_build\bin\ffplay.exe
 unzip %MEDIAINFO% MediaInfo.exe
-move ffmpeg-latest-win%ARCH%-shared\bin\*.* ..\..\..\bin\
+move ffmpeg-4.4-full_build\bin\*.* ..\..\..\bin\
 move MediaInfo.exe ..\..\..\bin\
 cd ..
 
@@ -53,11 +53,11 @@ REM ......................run pyinstaller......................
 
 if exist "dist\vidcutter.exe" (
     REM ......................add metadata to built Windows binary......................
-    .\verpatch.exe dist\vidcutter.exe /va %APPVER%.0 /pv %APPVER%.0 /s desc "VidCutter" /s name "VidCutter" /s copyright "(c) 2020 Pete Alexandrou" /s product "VidCutter %BINARCH%" /s company "ozmartians.com"
+    .\verpatch.exe dist\vidcutter.exe /va %APPVER%.0 /pv %APPVER%.0 /s desc "VidCutter" /s name "VidCutter" /s copyright "(c) 2021 Pete Alexandrou" /s product "VidCutter %BINARCH%" /s company "ozmartians.com"
 
     REM ......................call Inno Setup installer build script......................
     cd ..\InnoSetup
-    "C:\Program Files (x86)\Inno Setup 5\iscc.exe" installer_%BINARCH%.iss
+    "C:\Program Files (x86)\Inno Setup 6\iscc.exe" installer_%BINARCH%.iss
 
     cd ..\pyinstaller
 )
--- _build/pyinstaller/build.pyinstaller.win.signed.bat
+++ _build/pyinstaller/build.pyinstaller.win.signed.bat
@@ -17,24 +17,22 @@ if [%2]==[] (
 
 if ["%ARCH%"]==["64"] (
     SET BINARCH=x64
-    SET PYPATH=C:\Python36-x64
-    REM SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win64/shared/ffmpeg-latest-win64-shared.zip
-    SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win64/shared/ffmpeg-4.0.2-win64-shared.zip
-    REM SET FFMPEG=ffmpeg-latest-win64-shared.zip
-    SET FFMPEG=ffmpeg-4.0.2-win64-shared.zip
-    SET MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/18.05/MediaInfo_CLI_18.05_Windows_x64.zip
-    SET MEDIAINFO=MediaInfo_CLI_18.05_Windows_x64.zip
+    SET PYPATH=C:\Python39
+    SET FFMPEG_URL=https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-4.4-full_build.7z
+    SET FFMPEG=ffmpeg-4.4-full_build.7z
+    SET MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/21.03/MediaInfo_CLI_21.03_Windows_x64.zip
+    SET MEDIAINFO=MediaInfo_CLI_21.03_Windows_x64.zip
 )
-if ["%ARCH%"]==["32"] (
-    SET BINARCH=x86
-    SET PYPATH=C:\Python36
-    REM SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win32/shared/ffmpeg-latest-win32-shared.zip
-    SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win32/shared/ffmpeg-4.0.2-win32-shared.zip
-    REM SET FFMPEG=ffmpeg-latest-win32-shared.zip
-    SET FFMPEG=ffmpeg-4.0.2-win32-shared.zip
-    SET MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/18.05/MediaInfo_CLI_18.05_Windows_i386.zip
-    SET MEDIAINFO=MediaInfo_CLI_18.05_Windows_i386.zip
-)
+rem if ["%ARCH%"]==["32"] (
+rem     SET BINARCH=x86
+rem     SET PYPATH=C:\Python36
+rem     REM SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win32/shared/ffmpeg-latest-win32-shared.zip
+rem     SET FFMPEG_URL=https://ffmpeg.zeranoe.com/builds/win32/shared/ffmpeg-4.0.2-win32-shared.zip
+rem     REM SET FFMPEG=ffmpeg-latest-win32-shared.zip
+rem     SET FFMPEG=ffmpeg-4.0.2-win32-shared.zip
+rem     SET MEDIAINFO_URL=https://mediaarea.net/download/binary/mediainfo/18.05/MediaInfo_CLI_18.05_Windows_i386.zip
+rem     SET MEDIAINFO=MediaInfo_CLI_18.05_Windows_i386.zip
+rem )
 
 REM ......................get latest version number......................
 for /f "delims=" %%a in ('%PYPATH%\python.exe version.py') do @set APPVER=%%a
@@ -51,10 +49,10 @@ if not exist "temp\%MEDIAINFO%" ( call curl -k -L -# -o temp\%MEDIAINFO% "%MEDIA
 
 REM ......................extract files & move them to top-level binary folder ......................
 cd temp\
-7z x "%FFMPEG%" ffmpeg-4.0.2-win%ARCH%-shared\bin
-del /q ffmpeg-4.0.2-win%ARCH%-shared\bin\ffplay.exe
+7z x "%FFMPEG%" ffmpeg-4.4-full_build\bin
+del /q ffmpeg-4.4-full_build\bin\ffplay.exe
 unzip "%MEDIAINFO%" MediaInfo.exe
-move ffmpeg-4.0.2-win%ARCH%-shared\bin\*.* ..\..\..\bin\
+move ffmpeg-4.4-full_build\bin\*.* ..\..\..\bin\
 move MediaInfo.exe ..\..\..\bin\
 cd ..
 
@@ -63,14 +61,14 @@ REM ......................run pyinstaller......................
 
 if exist "dist\vidcutter.exe" (
     REM ......................add metadata to built Windows binary......................
-    .\verpatch.exe dist\vidcutter.exe /va %APPVER%.0 /pv %APPVER%.0 /s desc "VidCutter" /s name "VidCutter" /s copyright "(c) 2020 Pete Alexandrou" /s product "VidCutter %BINARCH%" /s company "ozmartians.com"
+    .\verpatch.exe dist\vidcutter.exe /va %APPVER%.0 /pv %APPVER%.0 /s desc "VidCutter" /s name "VidCutter" /s copyright "(c) 2021 Pete Alexandrou" /s product "VidCutter %BINARCH%" /s company "ozmartians.com"
 
     REM ................sign frozen EXE with self-signed certificate..........
     SignTool.exe sign /f "..\certs\code-sign.pfx" /t http://timestamp.comodoca.com/authenticode /p %PASS% dist\vidcutter.exe
 
     REM ......................call Inno Setup installer build script......................
     cd ..\InnoSetup
-    call "C:\Program Files (x86)\Inno Setup 5\iscc.exe" installer_%BINARCH%.iss
+    call "C:\Program Files (x86)\Inno Setup 6\iscc.exe" installer_%BINARCH%.iss
 
     REM ................sign final redistributable EXE with self-signed certificate..........
     SignTool.exe sign /f "..\certs\code-sign.pfx" /t http://timestamp.comodoca.com/authenticode /p %PASS% output\VidCutter-%APPVER%-setup-win%ARCH%.exe
