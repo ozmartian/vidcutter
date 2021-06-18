64cadb49b23e048c0d6349c0865f32cf12d8df71
Pete Alexandrou <pete@ozmartians.com>
2021-06-18 18:13:54 +1000

    revert osx pyinstaller test

--- _packaging/homebrew/vidcutter.rb
+++ _packaging/homebrew/vidcutter.rb
@@ -1,9 +1,9 @@
 cask 'vidcutter' do
-  version '6.0.2'
+  version '6.0.5.1'
   sha256 'cebc95bd66d5df7fb46831bdd12993a9738ea1528850275e09a3ca69446851a7'
 
   # github.com/ozmartian/vidcutter was verified as official when first introduced to the cask
-  url "https://github.com/ozmartian/vidcutter/releases/download/#{version.major_minor}.0/VidCutter-#{version}-macOS.dmg"
+  url "https://github.com/ozmartian/vidcutter/releases/download/#{version.major_minor}/VidCutter-#{version}-macOS.dmg"
   appcast 'https://github.com/ozmartian/vidcutter/releases.atom'
   name 'VidCutter'
   homepage 'https://vidcutter.ozmartians.com/'
